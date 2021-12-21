2b648ed31a40dbc54082e003f05806a0e8afba79
HADES <1005710179@qq.com>
2021-12-21 22:48:54 +0800

    登录

---
+++ src/views/sys/login/LoginForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="mb-3 text-2xl font-bold text-center xl:text-3xl enter-x xl:text-left dark:text-gray-100"
+  >
+    登录
+  </div>
+  <el-form class="enter-x xl:w-90 mt-2">
+    <el-form-item>
+      <el-input placeholder="请输入登录账号" />
+    </el-form-item>
+    <el-form-item>
+      <el-input placeholder="请输入登录账号" />
+    </el-form-item>
+    <el-form-item class="enter-x">
+      <div class="flex justify-between">
+        <el-checkbox>记住密码</el-checkbox>
+        <el-button type="text" size="medium">忘记密码</el-button>
+      </div>
+    </el-form-item>
+    <el-form-item class="enter-x">
+      <el-button type="primary" size="medium" style="width: 100%"
+        >登录</el-button
+      >
+    </el-form-item>
+    <el-row :gutter="12">
+      <el-col :md="8" :xs="24">
+        <el-button style="width: 100%">手机登录</el-button></el-col
+      >
+      <el-col :md="8" :xs="24" class="!my-2 !md:my-0">
+        <el-button style="width: 100%">二位码登录</el-button></el-col
+      >
+      <el-col :md="8" :xs="24">
+        <el-button style="width: 100%">注册</el-button></el-col
+      >
+    </el-row>
+
+    <el-divider></el-divider>
+
+    <div
+      class="flex justify-evenly enter-x text-dark-100 dark:text-light-50 text-2xl"
+    >
+      <GithubFilled />
+      <WechatFilled />
+      <AlipayCircleFilled />
+      <GoogleCircleFilled />
+      <TwitterCircleFilled />
+    </div>
+  </el-form>
+</template>
+
+<script lang="ts" setup>
+import { BeakerIcon } from '@heroicons/vue/solid'
+import {
+  GithubFilled,
+  WechatFilled,
+  AlipayCircleFilled,
+  GoogleCircleFilled,
+  TwitterCircleFilled
+} from '@ant-design/icons-vue'
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/sys/login/index.vue
+++ src/views/sys/login/index.vue
@@ -1,11 +1,48 @@
 <template>
   <div class="login relative w-full h-full px-4">
     <DarkModeToggle class="absolute top-3 right-7 enter-x" />
+
+    <div class="container relative h-full py-20 mx-auto sm:px-10">
+      <div class="flex h-full">
+        <div class="hidden min-h-full pl-4 mr-4 xl:flex xl:flex-col xl:w-6/12">
+          <div class="logo -enter-x">
+            <div class="text-light-50 text-4xl">admin</div>
+          </div>
+          <div class="mt-auto mb-auto">
+            <img
+              class="w-1/2 -enter-x"
+              src="@/assets/svg/login-box-bg.svg"
+              alt=""
+            />
+
+            <div class="mt-10 font-medium text-white -enter-x">
+              <span class="inline-block mt-4 text-3xl">后台管理系统</span>
+            </div>
+
+            <div
+              class="mt-5 font-normal text-white text-md dark:text-gray-500 -enter-x"
+            >
+              输入您的个人详细信息开始使用！
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex w-full h-full py-5 xl:h-auto xl:py-0 xl:my-0 xl:w-6/12"
+        >
+          <div
+            class="relative login-form w-full px-5 py-8 mx-auto my-auto rounded-md shadow-md xl:ml-16 xl:bg-transparent sm:px-8 xl:p-4 xl:shadow-none sm:w-3/4 lg:w-2/4 xl:w-auto enter-x"
+          >
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import DarkModeToggle from '@/components/DarkModeToggle/index.vue'
+import LoginForm from './LoginForm.vue'
 </script>
 
 <style lang="scss" scoped>
@@ -14,6 +51,9 @@ import DarkModeToggle from '@/components/DarkModeToggle/index.vue'
     background-color: #293146;
     &::before {
       background-image: url(@/assets/svg/login-bg-dark.svg);
+    }
+    .login-form {
+      background-color: #293146;
     }
   }
 }
@@ -33,6 +73,9 @@ import DarkModeToggle from '@/components/DarkModeToggle/index.vue'
     @media (max-width: $screen-xl) {
       background-image: url(@/assets/svg/login-bg-dark.svg);
     }
+  }
+  .login-form {
+    background-color: #fff;
   }
 }
 </style>
