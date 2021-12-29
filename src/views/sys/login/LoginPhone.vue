f5d7c6de107dcd6d2a27a73d83dbdcece5b0fc68
HADES <1005710179@qq.com>
2021-12-29 13:03:43 +0800

    表单验证

--- src/views/sys/login/LoginPhone.vue
+++ src/views/sys/login/LoginPhone.vue
@@ -1,20 +1,35 @@
 <template>
   <template v-if="getShow">
     <LoginTitle />
-    <el-form class="enter-x xl:w-90 mt-2">
-      <el-form-item>
-        <el-input :placeholder="$t('sys.login.placeholderMobile')" />
+    <el-form
+      class="enter-x xl:w-90 mt-2"
+      :model="formData"
+      :rules="getFormRules"
+      ref="formRef"
+    >
+      <el-form-item class="enter-x" prop="mobile">
+        <el-input
+          v-model="formData.mobile"
+          :placeholder="$t('sys.login.placeholderMobile')"
+        />
       </el-form-item>
-      <el-form-item class="enter-x">
+      <el-form-item class="enter-x" prop="code">
         <div class="flex">
-          <el-input :placeholder="$t('sys.login.placeholderCode')" />
+          <el-input
+            v-model="formData.code"
+            :placeholder="$t('sys.login.placeholderCode')"
+          />
           <CountdownBtn />
         </div>
       </el-form-item>
       <el-form-item class="enter-x">
-        <el-button type="primary" size="medium" class="w-full">{{
-          $t('sys.login.signInFormTitle')
-        }}</el-button>
+        <el-button
+          type="primary"
+          size="medium"
+          class="w-full"
+          @click="handleLogin"
+          >{{ $t('sys.login.signInFormTitle') }}</el-button
+        >
       </el-form-item>
       <el-form-item class="enter-x">
         <el-button size="medium" class="w-full" @click="handleBack">{{
@@ -27,12 +42,31 @@
 
 <script lang="ts" setup>
 import LoginTitle from './LoginTitle.vue'
-import { computed, unref } from 'vue'
-import { LoginStateEnum, useLoginState } from './useLogin'
-
+import { computed, unref, reactive, ref } from 'vue'
+import {
+  LoginStateEnum,
+  useLoginState,
+  useFormRules,
+  useFormValid
+} from './useLogin'
+import { CountdownBtn } from '@/components/CountdownBtn'
 const { getLoginState, handleBack } = useLoginState()
-
 const getShow = computed(() => unref(getLoginState) === LoginStateEnum.MOBILE)
 
-import { CountdownBtn } from '@/components/CountdownBtn'
+const formRef = ref()
+
+const { getFormRules } = useFormRules()
+
+const formData = reactive({
+  mobile: '',
+  code: ''
+})
+
+const { validForm } = useFormValid(formRef)
+
+async function handleLogin() {
+  const data = await validForm()
+  if (!data) return
+  console.log(data)
+}
 </script>
--- src/views/sys/login/useLogin.ts
+++ src/views/sys/login/useLogin.ts
@@ -1,3 +1,5 @@
+import { useI18n } from '@/hooks/web/useI18n'
+import type { FormItemRule } from 'element-plus/es/components/form/src/form.type'
 import { ref, computed, unref, Ref } from 'vue'
 
 export enum LoginStateEnum {
@@ -22,4 +24,55 @@ export function useLoginState() {
   }
 
   return { setLoginState, getLoginState, handleBack }
+}
+
+export function useFormRules(formData?: Recordable) {
+  const { t } = useI18n()
+
+  const getCodeFormRule = computed(() =>
+    createRule(t('sys.login.placeholderCode'))
+  )
+  const getMobileFormRule = computed(() =>
+    createRule(t('sys.login.placeholderMobile'))
+  )
+
+  const getFormRules = computed(
+    (): { [k: string]: FormItemRule | FormItemRule[] } => {
+      const codeFormRule = unref(getCodeFormRule)
+      const mobileFormRule = unref(getMobileFormRule)
+
+      switch (unref(loginState)) {
+        case LoginStateEnum.MOBILE:
+          return {
+            code: codeFormRule,
+            mobile: mobileFormRule
+          }
+      }
+
+      return {}
+    }
+  )
+
+  return { getFormRules }
+}
+
+export function useFormValid<T extends Object = any>(formRef: Ref<any>) {
+  async function validForm() {
+    const form = unref(formRef)
+    if (!form) return
+    const data = await form.validate()
+    return data as T
+  }
+
+  return { validForm }
+}
+
+function createRule(message: string) {
+  return [
+    {
+      required: true,
+      message,
+      trigger: 'change'
+    }
+  ]
 }
